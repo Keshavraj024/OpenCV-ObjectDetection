9dbc4be226e58f9c4bbdead5afb3c85f787ecabe
keshav <keshavraj024@gmail.com>
2023-08-02 08:46:27 +0200

    add requirements

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# OpenCV-ObjectDetection
+# Face and Eye Detection
 
-This project includes the face and eye detection using the cascade classifier from OpenCV
+A Python script to perform face and eye detection using OpenCV's Haar Cascades.
+
+## Requirements
+
+- Run the command
+    
+    ```pip3 install -r requirements.txt```
+
+## Usage
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/Keshavraj024/OpenCV-ObjectDetection.git
+cd OpenCV-ObjectDetection
+```
+2. Download the Haar Cascade classifiers for face and eye detection from OpenCV:
+
+    - facedetection.xml
+
+    - eye.xml
+
+    - Place the classifiers in the same directory as the script
+
+3. Run the script to start the face and eye detection:
+
+    ```python facedetection_opencv.py```
+
--- facedetection_opencv.py
+++ facedetection_opencv.py
@@ -1,42 +1,35 @@
-"""
-Face and Eye detection using OpenCV
-"""
 import cv2
 
+
 class FaceDetection:
-	def __init__(self):
-		self.face_cascade = cv2.CascadeClassifier('facedetection.xml')
-		self.eye_cascade = cv2.CascadeClassifier('eye.xml')
-		self.font = cv2.FONT_HERSHEY_SIMPLEX
-		self.cap = cv2.VideoCapture(0)
+    def __init__(self) -> None:
+        """Initialize the FaceDetection class"""
+        self.face_cascade = cv2.CascadeClassifier("facedetection.xml")
+        self.eye_cascade = cv2.CascadeClassifier("eye.xml")
 
+    def _video_capture_loop(self) -> None:
+        """Capture video and perform face and eye detection."""
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            _, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=3)
+            eyes = self.eye_cascade.detectMultiScale(gray)
+            for x, y, w, h in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 5)
+                for x1, y1, w, h in eyes:
+                    cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (120, 0, 255), 5)
+            cv2.imshow("Face and Eye Detection", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
 
-	def detection(self):
-		while (self.cap.isOpened()):
-			# Capture the frame
-			ret,frame = self.cap.read()
-			# Convert the image to grayscale
-			gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-			# Get the coordinates of the face
-			faces = self.face_cascade.detectMultiScale(gray,1.4,3)
-			# Get the eye coordinates
-			eyes = self.eye_cascade.detectMultiScale(gray)
-			# Iterate through the coordinates
-			for (x,y,w,h) in faces:
-				# Draw the rectangle on the frame
-				cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,255),5)
-				# Put the text around the frame
-				#cv2.putText(frame,"Obj",(x-10,y-10),self.font,1,(255,0,0),5,cv2.LINE_AA)
-				# Iterate through the eye coordinate
-				for x1,y1,w,h in eyes:
-					cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(255,0,255),5)
-			cv2.imshow("Result",frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-		self.cap.release()
-		cv2.destroyAllWindows()
+    def detection(self) -> None:
+        """Run the face and eye detection algorithm."""
+        self._video_capture_loop()
+
 
 if __name__ == "__main__":
-	detect = FaceDetection()
-	detect.detection()
-	
+    detect = FaceDetection()
+    detect.detection()
